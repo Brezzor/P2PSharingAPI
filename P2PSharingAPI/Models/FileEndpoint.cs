a368ebf96755ba970f37c4428ca71ef4a5787cf2
Brezzor <oliv095w@edu.zealand.dk>
2023-03-21 14:19:30 +0100

    Add project files.

---
+++ P2PSharingAPI/Models/FileEndpoint.cs
@@ -0,0 +1,9 @@
+﻿namespace P2PSharingAPI.Models
+{
+    public class FileEndpoint
+    {
+        public string? FileName { get; set; }
+        public string? Ip { get; set; }
+        public int Port { get; set; }
+    }
+}
